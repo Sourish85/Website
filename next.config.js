e64db00c0c433c6d3a2c09fc70fda864161f07c5
Sourish Suri <sourishwb@gmail.com>
2025-08-28 17:29:17 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   reactStrictMode: true,
   output: "export",
+  basePath: isProd ? '/Website' : '',
+  assetPrefix: isProd ? '/Website/' : '',
 };
 
 module.exports = nextConfig;
